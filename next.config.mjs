64c5cf6161a5b04c564b3c1673c7482ade001c8b
felipe <jesusbecerra91@gmail.com>
2025-03-24 19:03:00 -0300

    Actualización de configuraciones y archivos del frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone',
+    // Otras configuraciones
+  };
+  
+  export default nextConfig;
